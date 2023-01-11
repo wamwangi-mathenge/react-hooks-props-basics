5e8f818e1490201c2a4ce2d3ff6d655ed2751efc
Brian <wamwangimathenge@gmail.com>
2023-01-11 19:18:02 +0300

    Additional Comments Made - Not So Nice

--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -9,6 +9,9 @@ function BlogPost() {
       <Comment commentText="I agree with this statement. - Angela Merkel" />
       <Comment commentText="A universal truth. - Noam Chomsky" />
       <Comment commentText="Truth is singular. Its ‘versions’ are mistruths. - Sonmi-451" />
+      <Comment commentText="That's BullShit!!! - Jurgen" />
+      <Comment commentText="I don't care what y'all think. Brian's learning React!!! - Pep" />
+      <Comment commentText="Very Nice, Very Nice - Maxine :) " />
     </div>
   );
 }
